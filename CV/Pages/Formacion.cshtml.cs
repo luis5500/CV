b3757c2b90bb9a45afca20c614266e690e1c5f34
luis5500 <luisgarciagascon@gmail.com>
2025-06-08 19:59:13 +0200

    Inicio del proyecto. Plantillas para contacto, experiencia, formacion y sobre mi

---
+++ CV/Pages/Formacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages
+{
+    public class FormacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
