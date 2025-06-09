230643ad10a579956bf69817374101a5a3abbbb8
luis5500 <luisgarciagascon@gmail.com>
2025-06-09 22:10:57 +0200

    Added some projects in Proyectos

---
+++ CV/Models/ProyectoDestacado.cs
@@ -0,0 +1,9 @@
+﻿namespace CV.Models
+{
+    public class ProyectoDestacado
+    {
+        public string Nombre { get; set; }
+        public string LogoUrl { get; set; }
+        public string Enlace { get; set; }
+    }
+}
